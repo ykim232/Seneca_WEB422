8754c94776e6ffe7b03181b8de0a6d2b167e65a7
Yuli Kim <ykim232@myseneca.ca>
2022-01-21 16:03:44 +0900

    [Update] Final

--- server.js
+++ server.js
@@ -13,11 +13,10 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const HTTP_PORT = process.env.PORT || 8080;
+const path = require('path');
 
 const RestaurantDB = require("./modules/restaurantDB.js");
 const db = new RestaurantDB();
-
-require("dotenv").config({ path : "variables.env"});
 
 db.initialize("mongodb+srv://ykim2323:gusdn0649@web422yuli.mhhdx.mongodb.net/web422yuli?retryWrites=true&w=majority").then(()=>{
   app.listen(HTTP_PORT, ()=>{
@@ -42,7 +41,7 @@ app.post("/api/restaurants", (req,res) => {
           res.status(201).json(`new Restaurant successfully added`);
       })
       .catch((err) => {
-          res.status(400).json(err);
+        res.status(500).json(`Error has occured : ${err}`);
       });
 });
 
@@ -51,9 +50,9 @@ app.get("/api/restaurants", (req,res) => {
   db.getAllRestaurants(req.query.page, req.query.perPage, req.query.borough)
       .then((restaurants) => {
           res.status(200).json(restaurants);
-      })
+      }) 
       .catch((err) => {
-          res.status(400).json(err);
+        res.status(500).json(`Error has occured : ${err}`);
       });
 });
 
@@ -64,7 +63,7 @@ app.get("/api/restaurants/:_id", (req,res) => {
           res.status(200).json(restaurants);
       })
       .catch((err) => {
-          res.status(404).json(err);
+        res.status(500).json(`Error has occured : ${err}`);
       });
 });
 
@@ -75,7 +74,7 @@ app.put("/api/restaurants/:_id", (req,res) => {
           res.status(200).json(`Restaurant ${req.body._id} successfully updated`);
       })
       .catch((err) => {
-          res.status(404).json(err);
+          res.status(500).json(`Error has occured : ${err}`);
       });
 });
 
@@ -85,6 +84,6 @@ app.delete("/api/restaurants/:_id", (req,res) => {
           res.status(200).json(`Restaurant ${req.params._id} successfully deleted`);
       })
       .catch((err) => {
-          res.status(404).json(err);
+          res.status(500).json(`Error has occured : ${err}`);
       });
 });
