f0b80d10b37c479f9e480437ba3fb80aa75baeb4
edes0830 <edes0830@gmail.com>
2022-02-03 23:48:25 +0900

    [Edit] celebrate part2

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.post("/api/restaurants", celebrate({
   [Segments.QUERY]: Joi.object().keys({
     page: Joi.string().required(),
     perPage: Joi.number().required(),
-    borough: Joi.string().required
+    borough: Joi.string().required()
   })
 }),
   (req, res) => {
