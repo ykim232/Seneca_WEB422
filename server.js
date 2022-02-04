ea0be5e39ad470a2ff350f2927e2d56c8fcc5997
Yuli Kim <ykim232@myseneca.ca>
2022-02-05 02:49:29 +0900

    [update] changed dbname

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const db = new RestaurantDB();
 require("dotenv").config({ path: "./config/config.env" });
 
 // db.initialize(`mongodb+srv://${process.env.dbUser}:${process.env.dbPass}@cluster0-apgkj.mongodb.net/${process.env.dbName}?retryWrites=true&w=majority`)
-db.initialize(`mongodb+srv://ykim2323:gusdn0649@web422yuli.mhhdx.mongodb.net/sample_restaurnants?retryWrites=true&w=majority`)
+db.initialize("mongodb+srv://ykim2323:gusdn0649@web422yuli.mhhdx.mongodb.net/sample_restaurants?retryWrites=true&w=majority")
 .then(() => {
   app.listen(process.env.PORT, () => {
     console.log(`server listening`);
