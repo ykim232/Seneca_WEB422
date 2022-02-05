8c71e83c85cfde8efd1d9255cad942b0ffc408e2
Yuli Kim <92243410+ykim232@users.noreply.github.com>
2022-02-05 12:25:35 +0900

    Update server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get("/api/restaurants", celebrate({
   [Segments.QUERY]: Joi.object().keys({
     page: Joi.number().required(),
     perPage: Joi.number().required(),
-//     borough: Joi.string().required()
+    borough: Joi.string()
   })
 }), (req, res) => {
   db.getAllRestaurants(req.query.page, req.query.perPage, req.query.borough)
